49edbc294e44781bb90cb173fe81016b2c863c3b
yhk1038 <yhk1038@hanmail.net>
2015-12-27 05:03:05 +0900

    기능 테스트가 작업 아이템을 추가할 수 있는지 확인

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,18 +19,33 @@ class NewVisitorTest(unittest.TestCase):
         
         # �� ������ Ÿ��Ʋ�� ������ 'To-Do'�� ǥ���ϰ� �ִ�.
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
         # �׳��� �ٷ� �۾��� �߰��ϱ��� �Ѵ�.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
         
         # "���۱��� ����"���� �ؽ�Ʈ ���ڿ� �Է��Ѵ�
         # (�������� ���̴� ��ġ ���̿� �׹��� ������ ���̴�)
+        inputbox.send_keys('공작깃털 사기')
         
         # ����Ű�� ġ�� �������� ���ŵǰ� �۾� ���Ͽ�
         # "1: ���۱��� ����" �������� �߰��ȴ�
+        inputbox.send_keys(Keys.ENTER)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
         
         # �߰� �������� �Է��� �� �ִ� ������ �ؽ�Ʈ ���ڰ� �����Ѵ�
         # �ٽ� "���۱����� �̿��ؼ� �׹� ������"���� �Է��Ѵ�. (���𽺴� �ſ� ü������ �����̴�.)
+        self.fail('Finish the test!')
         
         # �������� �ٽ� ���ŵǰ�, �� �� �������� ���Ͽ� ���δ�
         # ���𽺴� ����Ʈ�� �Է��� ������ �����ϰ� �ִ��� �ñ��ϴ�
