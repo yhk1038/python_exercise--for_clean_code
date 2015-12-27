024a7add6c0c9565adf54f3992ed61677d4fcaa7
yhk1038 <yhk1038@hanmail.net>
2015-12-27 10:40:10 +0900

    "before refactoring"

--- lists/tests.py
+++ lists/tests.py
@@ -14,8 +14,20 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.strip().endswith(b'</html>'))
+        
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+        
+        response = home_page(request)
+        
+        self.assertIn('신규 작업 아이템', response.content.decode())
