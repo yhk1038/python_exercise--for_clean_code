76cd882384c50e2461c1d4a005f3cd233c3b5c32
yhk1038 <yhk1038@hanmail.net>
2015-12-30 17:29:44 +0900

    chapter6 complete

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,8 @@
 from django.core.urlresolvers import resolve
-from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.test import TestCase
+
 from lists.models import Item, List
 from lists.views import home_page
 
@@ -11,22 +12,100 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
         
+        
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
-        request.method = 'POST'
-        request.POST["item_text"] = "신규 작업 아이템"
         response = home_page(request)
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': '신규 작업 아이템'}
+        expected_html = render_to_string('home.html')
+        # self.assertEqual(response.content.decode(), expected_html)
+        
+        
+        
+class NewListTest(TestCase):
+    
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
         )
-        # self.assertEqual(expected_html, response.content.decode())
-        # self.assertTrue(response.content.startswith(b'<html>'))
-        # self.assertIn(b'<title>To-Do lists</title>', response.content)
-        # self.assertTrue(response.content.strip().endswith(b'</html>'))
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
         
         
-class ListAndItemModelTest(TestCase):
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
+        )
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+        
+        
+        
+class NewItemTest(TestCase):
+    
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': '기존 목록에 신규 아이템'}
+        )
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
+        self.assertEqual(new_item.list, correct_list)
+        
+        
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': '기존 목록에 신규 아이템'}
+        )
+        
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+        
+        
+        
+class ListViewTest(TestCase):
+    
+    def test_uses_list_template(self):
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id,))
+        self.assertTemplateUsed(response, 'list.html')
+        
+        
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
+        
+        
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='다른 목록 아이템 1', list=other_list)
+        Item.objects.create(text='다른 목록 아이템 2', list=other_list)
+        
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, '다른 목록 아이템 1')
+        self.assertNotContains(response, '다른 목록 아이템 2')
+        
+        
+        
+class ListAndItemModelsTest(TestCase):
     
     def test_saving_and_retrieving_items(self):
         list_ = List()
@@ -46,48 +125,13 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(saved_list, list_)
         
         saved_items = Item.objects.all()
-        #saved_items에 2개가 잘 들어갔는지 check
-        self.assertEqual(saved_items.count(), 2)    
+        self.assertEqual(saved_items.count(), 2)
         
-        #saved_items를 배열로 봤을때 첫 object를 first_saved_item로 명명.
         first_saved_item = saved_items[0]
-        #saved_items를 배열로 봤을때 두번째 object를 second_saved_item로 명명.
         second_saved_item = saved_items[1]
-        #각각 정의된 것들이 의도된대로 정의됐는지 check
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
         self.assertEqual(second_saved_item.list, list_)
         
-class ListViewTest(TestCase):
-    
-    def test_saving_a_POST_request(self):
-        self.client.post(
-            '/lists/new',
-            data={'item_text': '신규 작업 아이템'}
-        )
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, '신규 작업 아이템')
         
-    def test_redirects_after_POST(self):
-        response = self.client.post(
-            '/lists/new',
-            data={'item_text': '신규 작업 아이템'}
-        )
-        self.assertRedirects(response, '/lists/the_only_list_in_the_world/')
-    
-    
-    def test_users_list_template(self):
-        response = self.client.get('/lists/the_only_list_in_the_world/')
-        self.assertTemplateUsed(response, 'list.html')
-    
-    def test_displays_all_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
-        
-        response = self.client.get('/lists/the_only_list_in_the_world/')
-        
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
--- lists/urls.py
+++ lists/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),
-    # url(r'^admin/', admin.site.urls),
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
 ]
