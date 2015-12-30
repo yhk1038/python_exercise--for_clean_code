e18b0dff5ab0f0e91a6e3f392affa535c38c320e
yhk1038 <yhk1038@hanmail.net>
2015-12-30 14:46:46 +0900

    remake migration.py

--- lists/tests.py
+++ lists/tests.py
@@ -2,8 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
-
+from lists.models import Item, List
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -27,28 +26,38 @@ class HomePageTest(TestCase):
         # self.assertTrue(response.content.strip().endswith(b'</html>'))
         
         
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
         first_item = Item()
         first_item.text = '첫 번째 아이템'
+        first_item.list = list_
         first_item.save()
         
         second_item = Item()
         second_item.text = '두 번째 아이템'
+        second_item.list = list_
         second_item.save()
+        
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
         
         saved_items = Item.objects.all()
         #saved_items에 2개가 잘 들어갔는지 check
         self.assertEqual(saved_items.count(), 2)    
         
         #saved_items를 배열로 봤을때 첫 object를 first_saved_item로 명명.
-        first_saved_item = saved_items[0]           
+        first_saved_item = saved_items[0]
         #saved_items를 배열로 봤을때 두번째 object를 second_saved_item로 명명.
         second_saved_item = saved_items[1]
         #각각 정의된 것들이 의도된대로 정의됐는지 check
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
         
 class ListViewTest(TestCase):
     
@@ -74,8 +83,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
     
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
         
         response = self.client.get('/lists/the_only_list_in_the_world/')
         
