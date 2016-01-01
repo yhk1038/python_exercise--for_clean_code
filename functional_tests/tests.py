27b5ff3c7eb18f5a28c887ce3728d2c9552697d8
yhk1038 <yhk1038@hanmail.net>
2016-01-01 23:33:56 +0900

    스테이징 테스트를 위한 FT 실행자 해킹

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,8 +2,23 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+    
+    @classmethod
+    def thearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
     
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고;
         # 해당 웹 사이트를 확인하러 간다;
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다; 
         self.assertIn('To-Do', self.browser.title)
@@ -65,7 +80,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         
         # 프란시스가 홈페이지에 접속한다;
         # 에디스의 리스트는 보이지 않는다;
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('공작깃털 사기', page_text)
         self.assertNotIn('그물 만들기', page_text)
@@ -90,15 +105,15 @@ class NewVisitorTest(StaticLiveServerTestCase):
         
     def test_layout_and_styling(self):
         # 에디스는 메인 페이지를 방문한다;
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
         
         # 그녀는 입력 상자가 가운데 배치된 것을 본다;
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
+            502,
+            delta=5
         )
         
         # 그녀는 새로운 리스트를 시작하고 입력상자가
@@ -107,10 +122,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
+            502,
+            delta=5
         )
-        
-        
-# if __name__ == '__main__':
-#     unittest.main(warnings='ignore')
